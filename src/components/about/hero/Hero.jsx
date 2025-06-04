025f596f2cdf645302fc5154b3776700c496a68a
dagmawinebeyu3 <dagmawinebeyu3@gmail.com>
2025-06-04 08:18:08 +0000

    Shorten title

--- src/components/about/hero/Hero.jsx
+++ src/components/about/hero/Hero.jsx
@@ -4,10 +4,10 @@ import Breadcrumb from "../../common/Breadcrumb/Breadcrumb";
 
 export default function Hero() {
   return (
-    <div className="about-wrapper" aria-labelledby="about-title">
+    <div className="about-wrapper">
       <Breadcrumb links={["home", "about"]} />
       <div className="about-main">
-        <h1 id="about-title">About Rainbow Computer Accessories and Stationery Supplier</h1>
+        <h1 id="about-title">About Us</h1>
         <p>
           Rainbow Computer Accessories and Stationery Supplier, with over 25 years of experience, is committed to providing businesses and individuals with high-quality office supplies. Since its founding, the company has built a strong reputation for reliability and customer service. Rainbow offers a wide range of products to enhance productivity and support the success of clients across various industries in Ethiopia.
         </p>
